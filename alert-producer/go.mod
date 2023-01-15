17ac24abf068367fbf832a33ebfdc46693e5fe18
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-01-15 12:53:02 -0500

    Basic project structure for POC with very basic checking to be later expanded on and refactored.

--- alert-producer/go.mod
+++ alert-producer/go.mod
@@ -5,6 +5,8 @@ go 1.17
 require github.com/gin-gonic/gin v1.8.2
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
@@ -16,6 +18,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/jdboisvert/kenny-u-pull-go-sdk v0.1.3
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -24,9 +27,9 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
-	golang.org/x/net v0.4.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- alert-producer/main.go
+++ alert-producer/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"reflect"
@@ -8,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-co-op/gocron"
+	kennyupull "github.com/jdboisvert/kenny-u-pull-go-sdk"
 )
 
 type Vehicle struct {
@@ -27,6 +30,67 @@ func vehicleAlreadySubscribed(vehiclesToSearchFor []Vehicle, vehicleToAdd Vehicl
 	}
 
 	return false
+}
+
+func checkVehiclesListings() {
+	// TODO call the API and check for new vehicles
+	log.Println("Checking for new vehicles to alert on...")
+	for _, vehicle := range vehiclesToSearchFor {
+		go checkForNewVehiclesPosted(vehicle)
+	}
+}
+
+func checkForNewVehiclesPosted(vehicle Vehicle) {
+	// TODO call the API and check for new vehicles
+
+	// TODO if new vehicles are found, send an alert to the alert consumer
+	inventorySearch := kennyupull.InventorySearch{
+		Year:  vehicle.Year,
+		Make:  vehicle.Make,
+		Model: vehicle.Model,
+	}
+	inventoryListings, err := kennyupull.GetInventory(inventorySearch)
+	if err != nil {
+		log.Println("Got an error when trying to get inventory listings: ", err)
+	}
+
+	log.Println("Got inventory listings: ", inventoryListings)
+	latestListing := inventoryListings[0]
+	log.Println("Latest listing: ", latestListing)
+
+	layout := "02-01-2006"
+	currentDate := time.Now()
+	listingTimestamp, _ := time.Parse(layout, latestListing.DateListed)
+
+	// TODO this needs to be smarter and handle the case where a new listing is posted multiple times a day
+	if listingTimestamp == currentDate {
+		// New listing found for today for the vehicle
+		log.Println("New listing found for vehicle: ", vehicle)
+		sendUpdateToSubscriber(latestListing)
+	}
+}
+
+func sendUpdateToSubscriber(latestListing kennyupull.InventoryListing) {
+	values := map[string]string{"year": latestListing.Year, "make": latestListing.Make, "model": latestListing.Model, "date_listed": latestListing.DateListed, "row_id": latestListing.RowID, "branch": latestListing.Branch, "listing_url": latestListing.ListingUrl}
+	json_data, err := json.Marshal(values)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO move to env var or config file and need retry logic for alerting via exponential backoff.
+	resp, err := http.Post("localhost:8080/v1/new-listing-consumer", "application/json",
+		bytes.NewBuffer(json_data))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("Got a non-200 status code from the alert consumer")
+	}
+
+	defer resp.Body.Close()
 }
 
 func subscribeToVehicle(c *gin.Context) {
@@ -52,9 +116,10 @@ func main() {
 	// TODO move to a separate file
 	// Run the scheduler to poll the API every 30 minutes
 	s := gocron.NewScheduler(time.UTC)
-	s.Every(30).Minutes().Do(func() {
-		// TODO call the API and check for new vehicles
-		log.Println("Checking for new vehicles to alert on...")
+
+	// For testing purposes, run every minute
+	s.Every(1).Minutes().Do(func() {
+		checkVehiclesListings()
 	})
 	s.StartAsync()
 }
