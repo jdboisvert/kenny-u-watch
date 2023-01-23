042aca406ce6e7a22919400e4c5f1ee8abf6bc06
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-01-22 21:16:31 -0500

    Started work for Docker set up with celery.

--- user_watch_management/user_watch/settings.py
+++ user_watch_management/user_watch/settings.py
@@ -141,11 +141,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # General TODO change to env variables
 
 # Celery related settings
-# CELERY_BROKER_URL = "redis://localhost:6379"
-# CELERY_RESULT_BACKEND = "redis://localhost:6379"
-# CELERY_ACCEPT_CONTENT = ["application/json"]
-# CELERY_RESULT_SERIALIZER = "json"
-# CELERY_TASK_SERIALIZER = "json"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = "json"
 
 # Email related settings
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
