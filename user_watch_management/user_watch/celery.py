b6f22336c404047fc5f33f89866b15e906c2cfd6
Jeffrey <info.jeffreyboisvert@gmail.com>
2023-01-24 18:31:45 -0800

    Added support for Docker and running tasks in celery.  (#4)

--- user_watch_management/user_watch/celery.py
+++ user_watch_management/user_watch/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_match.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_watch.settings")
 
-celery_app = Celery("user_match")
+celery_app = Celery("user_watch")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
